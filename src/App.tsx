59dc7b7f0cb2d61d4f8a3070b273880b0807b942
Angelo Cordon <angelo.cordon@hashicorp.com>
2024-09-16 17:12:34 -0700

    Set localhost server

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
+import './app.css';
 
-const NUMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const NUMS = [1, 2, 3, 4, 5, 6];
+
+const SIDES: { [key: number]: string } = {
+	1: '⚀',
+	2: '⚁',
+	3: '⚂',
+	4: '⚃',
+	5: '⚄',
+	6: '⚅',
+};
 
 function App() {
 	const [currentNum, setCurrentNum] = useState(NUMS[0]);
 
 	const randomNum = () => Math.floor(Math.random() * NUMS.length) + 1;
 
+	const renderCurrentSide = (num: number) => SIDES[num];
+
 	return (
 		<main>
-			<button onClick={() => setCurrentNum(randomNum())}>{currentNum}</button>
+			<button className="dice" onClick={() => setCurrentNum(randomNum())}>
+				{renderCurrentSide(currentNum)}
+			</button>
 		</main>
 	);
 }
