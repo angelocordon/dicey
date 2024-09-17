36317e89f926f6d4b70f85538f4b6c54f209a8d2
Angelo Cordon <angelo.cordon@hashicorp.com>
2024-09-16 18:16:06 -0700

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './app.css';
-
-const NUMS = [1, 2, 3, 4, 5, 6];
 
 const SIDES: { [key: number]: string } = {
 	1: '⚀',
@@ -12,16 +10,37 @@ const SIDES: { [key: number]: string } = {
 	6: '⚅',
 };
 
+const randomNum = () => Math.floor(Math.random() * 5) + 1;
+
 function App() {
-	const [currentNum, setCurrentNum] = useState(NUMS[0]);
+	const [currentNum, setCurrentNum] = useState(randomNum());
+	const intervalRef = useRef<number>();
 
-	const randomNum = () => Math.floor(Math.random() * NUMS.length) + 1;
+	const rollDice = () => {
+		setTimeout(() => {
+			intervalRef.current = setInterval(() => {
+				setCurrentNum(randomNum());
+			}, 150);
 
-	const renderCurrentSide = (num: number) => SIDES[num];
+			setTimeout(() => {
+				clearInterval(intervalRef.current);
+			}, 2000);
+		}, 0);
+	};
+
+	const renderCurrentSide = (num: number) => {
+		return SIDES[num];
+	};
+
+	useEffect(() => {
+		return () => {
+			clearInterval(intervalRef.current);
+		};
+	}, []);
 
 	return (
 		<main>
-			<button className="dice" onClick={() => setCurrentNum(randomNum())}>
+			<button className="dice" onClick={rollDice}>
 				{renderCurrentSide(currentNum)}
 			</button>
 		</main>
